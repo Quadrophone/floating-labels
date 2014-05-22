afa27a019924c8f898a50fcacd2d4a8fc91d73ba
James <james@quadrophone.com>
2014-05-22 12:39:18 -0700

    initial commit

---
+++ floating-labels.js
@@ -0,0 +1,31 @@
+(function($) {
+    $(document).ready(function() {
+        $('.floating').each(function() {
+            var inputLabel = $(this).attr('placeholder');
+            $('<span class="floaty-label">' + inputLabel + '</span>').insertBefore($(this)).hide();
+            var fontSize = parseInt($(this).css('font-size'));
+            var inputHeight = parseInt($(this).css('padding-top'));
+            var labelSize = inputHeight / 1.1;
+            var inputPadding = parseFloat($(this).css('padding-left'));
+            var inputPosition = $(this).position();
+            var labelLeft = inputPosition.left + inputPadding;
+            if ($(this).is('textarea')) {
+                var labelTop = inputPosition.top * 1.2;
+            } else {
+                var labelTop = inputPosition.top * 1.03;
+            }
+            $(this).prev('.floating-label').css({
+                'font-size': labelSize + 'px',
+                'left': labelLeft + 'px',
+                'top': labelTop + 'px'
+            });
+        });
+        $('.floating').on('keyup', function() {
+            if ($(this).val()) {
+                $(this).prev('.floating-label').show();
+            } else {
+                $(this).prev('.floating-label').hide();
+            }
+        });
+    });
+})(jQuery);
